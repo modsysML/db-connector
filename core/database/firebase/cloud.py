a2dbd9b56a07ca5d8f4189a0bcfff253f795c660
KenaMuigai <kenamuigai@gmail.com>
2023-11-17 21:35:07 +0300

    Update cloud.py
    <new>: Initialize, execute, exception (will add the logic as I continue to make changes)

--- core/database/firebase/cloud.py
+++ core/database/firebase/cloud.py
@@ -13,7 +13,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
+import json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
@@ -23,5 +23,36 @@ from .base import AbstractFirebaseClient
 
 class FirebaseClient(AbstractFirebaseClient):
     @staticmethod
-    def foo(argument):
-        return argument
+    def __init__(self, database_url, credentials_path):
+        self.database_url = database_url
+        self.credentials_path = credentials_path
+        self.initialize_firebase()
+
+    def initialize_firebase(self):
+        cred = credentials.Certificate(self.credentials_path)
+        firebase_admin.initialize_app(cred, {"databaseURL": self.database_url})
+
+    def execute(self, path, action="get", data=None):
+        if not path:
+            raise ValueError("Path is required for Firebase operations")
+
+        ref = db.reference(path)
+
+        try:
+            if action == "get":
+                return ref.get()
+            elif action == "set":
+                if data is None:
+                    raise ValueError("Data is required for 'set' action")
+                ref.set(json.loads(data))
+                return "Data set successfully"
+            elif action == "update":
+                if data is None:
+                    raise ValueError("Data is required for 'update' action")
+                ref.update(json.loads(data))
+                return "Data updated successfully"
+            else:
+                raise ValueError(f"Unsupported action: {action}")
+        except Exception as e:
+            # will add logic for this exception as I commit changes
+            raise e
